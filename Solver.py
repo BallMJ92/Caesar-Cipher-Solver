75453d868e12634748c6a5738b3a960101611fe5
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2017-12-14 14:36:48 +0000

    Update Solver.py

--- Solver.py
+++ Solver.py
@@ -7,24 +7,33 @@ class shiftSolver:
                8:'h', 9:'i', 10:'j', 11:'k', 12:'l', 13:'m', 14:'n',
                15:'o', 16:'p', 17:'q', 18:'r', 19:'s', 20:'t', 21:'u',
                22:'v', 23:'w', 24:'x', 25:'y', 26:'z'}
-
+        
+        #List to hold Ciphertext/Plaintext after user specified shift
         shift = []
-
+        
+        #Length of dictionary which contains the 26 characters of the alphabet
         length = int(len(dic))
-
+        
+        #for Ciphertext/Plaintext in list
         for x in c:
+            #for letter in Ciphertext/Plaintext
             for n in x:
+                #adding space if it appears in Ciphertext/Plaintext
                 if n == ' ':
                     shift.append(' ')
                 for y in range(1, length+1):
+                    #if character is listed in dictionary
                     if n == str(dic[y]):
+                        #if y+shifts equal 26 then y=shifts because a shift of 26 will not change letter
                         if y+shifts == 26:
                             y=shifts
+                            #if y+shifts greater than 26 then we continue to 26 and jump back to shift 1 and continue from there
                         elif y+shifts >=26:
                             restartShift = y+shifts-26
                             y=restartShift
                         else:
                             y+=shifts
+                        #appending specific value in dictionary with shift y applied to shift list
                         shift.append(dic[y])
 
         postShift = ''.join(shift)
@@ -34,16 +43,17 @@ class shiftSolver:
         crypt = []
         userInput = str(input("Please enter the Plaintext/Ciphertext: ")).lower()
         shiftInput = int(input("How many shifts do you want to implement: "))
-
+        
+        #A shift greater than 26 is no different to a shift below 26 because there are only 26 letters in the alphabet
         if shiftInput > 26:
             shiftInput = int(input("Please enter a shift between 1 and 25: "))
+        #A shift of 26 is the same as a shift of 0 because there are only 26 letters in the alphabet
         elif shiftInput == 26:
             print("Shifts by 26 will revert Ciphertext to it's original state")
         else:
             pass
 
         crypt.append(userInput)
-
         self.solve(crypt, shiftInput)
 
 if __name__ == "__main__":
