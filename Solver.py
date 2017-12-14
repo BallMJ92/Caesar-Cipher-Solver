5593f67c199520bfd091077673275a7a42ca20ba
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2017-12-14 14:39:41 +0000

    Update Solver.py

--- Solver.py
+++ Solver.py
@@ -2,7 +2,9 @@ class caesarSolver:
 
     def solve(self, c, s):
         shifts = int(s)
-
+        
+        """Dictionary to hold all letters in the alphabet along with their specific index within the alphabet
+        This allows easy access of index values and allows the program to simply apply the shifts through addition"""
         dic = {1:'a', 2:'b', 3:'c', 4:'d', 5:'e', 6:'f', 7:'g',
                8:'h', 9:'i', 10:'j', 11:'k', 12:'l', 13:'m', 14:'n',
                15:'o', 16:'p', 17:'q', 18:'r', 19:'s', 20:'t', 21:'u',
